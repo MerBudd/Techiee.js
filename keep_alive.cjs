a3d448f7c09c1205703f4c4ba5eea8cfd800b0df
MerBudd <82082386+MerBudd@users.noreply.github.com>
2024-02-22 13:51:11 +0300

    Updated webserver text and made it say the same thing to log.

--- keep_alive.cjs
+++ keep_alive.cjs
@@ -2,6 +2,7 @@
 
 var http = require('http');
 http.createServer(function (req, res) {
-  res.write("I'm alive, discord-gemini.merbudd.repl.co");
+  res.write("Webserver is on, 24/7 should work. https://techiee.onrender.com");
+  console.log("Webserver is on, 24/7 should work. https://techiee.onrender.com");
   res.end();
 }).listen(8080);
