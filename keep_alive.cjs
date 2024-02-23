3c3b15936bffe8e32354152afa391727002ad66a
MerBudd <82082386+MerBudd@users.noreply.github.com>
2024-02-23 21:39:47 +0300

    Update keep_alive.cjs

--- keep_alive.cjs
+++ keep_alive.cjs
@@ -1,9 +1,9 @@
 // Turn on webserver, log to console and webserver
 
-console.log("Webserver is on, 24/7 should work and bot should be alive. https://techiee.onrender.com");
+console.log("Webserver is on, 24/7 should work and bot should be alive.");
 
 var http = require('http');
 http.createServer(function (req, res) {
-  res.write("Webserver is on, 24/7 should work and bot should be alive. https://techiee.onrender.com");
+  res.write("Webserver is on, 24/7 should work and bot should be alive.");
   res.end();
 }).listen(8080);
