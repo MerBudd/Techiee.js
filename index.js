832ad7a2bd4eed727da895fbf61d90015b24a3bc
MerBudd <82082386+MerBudd@users.noreply.github.com>
2024-03-02 22:03:00 +0300

    reverted to old commit because the latest didn't work properly

--- index.js
+++ index.js
@@ -12,45 +12,42 @@ const API_KEY = process.env['gemini_api_key']
 const BOT_TOKEN = process.env['bot_token']
 const CHANNEL_ID = process.env['channel_id']
 
-// Gemini Configs... & stuff
 
-async function geminiAI(userInput) {
-try{
-    const safetySettings = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
+// Gemini Configs
+
+     const safetySettings = [
   {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_NONE,
-  },
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
     
 ];
 
-    const model = ai.getGenerativeModel({ 
-        model: MODEL,
-        safetySettings,
-    });
-    //Sets how Ai responds
-    const chat = model.startChat({
-        history: historyLog,
-        safetySettings,
-        generationConfig: {
-          temperature: 0.9,
-          topK: 1,
-          topP: 1,
-          maxOutputTokens: 2048,
-        },
-    });
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+
+// Something bout Gemini idunno
+
+const ai = new GoogleGenerativeAI(API_KEY);
+const model = ai.getGenerativeModel({ model: MODEL, safetySettings, generationConfig });
+
 
 // Bot
 
@@ -68,74 +65,68 @@ client.on("ready", () => {
 
 client.login(BOT_TOKEN);
 
-client.on('messageCreate', async (message) => {
-    //Checks for message sent by user, in correct channel, and history pattern
-    if(!message.author.bot && message.channel.id == channel && historyLog[historyLog.length-1].role != "user"){
-        //Checks for user > model pattern
-         if (historyLog[historyLog.length-1].role != "user"){
-            //Starts generation timer
-            const start = Date.now();
-            message.channel.send('*Generating*').then(msg => {setTimeout(() => msg.delete(), 2000)})
-            geminiAI(message.content).then(text => {
-                const end = Date.now();
-                let timeTaken = ((end-start)*.001).toFixed(3);
-                //Checks for empty message
-                if (text !== ""){
-                    //Checks Discord Message Limit
-                    if (text.length > 1999){
-                        let size = Math.ceil(text.length/2000);
-                        let half = Math.ceil(text.length/size);
-                        for(i = 0; i < size; i++){
-                            message.channel.send(text.substring(i*half,(i+1) *half));
-                        }
-                        message.channel.send("\n***Generation Time: " +timeTaken+ 's***');
-                    } else {
-                        message.channel.send(text + " \n***Generation Time: " +timeTaken + 's***')
-                    }
-                }
-            })
-        }
-    }
-})
+ client.on("messageCreate", async (message) => {
+  try{
+    if (message.author.bot) return;
+    if (message.channel.id !== CHANNEL_ID) return;
 
-
+       
 // Sets initial history
+    var historyLogI = [
 
-let historyLog = [
-  {
+
+    {
+      role: "user",
+          parts: "Hello!",
+            },
+    {
+      role: "model",
+          parts: "Hello! I am Techiee, an experimental chatbot built on Google's Gemini, developed by Tech and Budd. Or, your friendly neighborhood chatbot, as one might say. How can I help you?",
+            },
+                        ];
+
+// Sets user added history log
+ 
+    var historyLog = [
+
+    ];
+
+
+// Makes the final log
+
+   // var finalLog = historyLogI.concat(historyLog);
+
+       
+ // Limits history and removes first message from log.
+       
+     var limit = 30;
+     if (historyLog.length >  limit) {
+        historyLog.shift();
+        historyLog.shift();
+     }
+
+    const chat = model.startChat({
+     //history: finalLog,
+     history: historyLogI.concat(historyLog),
+     safetySettings,
+    });
+       
+const result = await chat.sendMessage(message.cleanContent);     
+const response = await result.response;
+const text = response.text();
+
+
+// Saves user input and bot output into history
+       
+  historyLog.push({
     role: "user",
-    parts: "Hello!",
-  },
-  {
+    parts: message,
+        });
+
+   historyLog.push({
     role: "model",
-    parts: "Hello! I am Techiee, an experimental chatbot built on Google's Gemini, developed by Tech and Budd. Or, your friendly neighborhood chatbot, as one might say. How can I help you?",
-  },
-];
-
-
-// Saves User input into history
-historyLog.push({
-     role: "user",
-     parts: userInput,
-    });
-    const msg = userInput;
-    const result = await chat.sendMessage(msg);
-    const response = await result.response;
-    const text = response.text();
-    //Saves Ai output into history
-    historyLog.push({
-        role: "model",
-        parts: text,
-    });
-     
-    // Limits history, Change limit to keep more history
-    let limit = 30;
-    if (historyLog.length >  limit) {
-        historyLog.shift();
-        historyLog.shift();
-    }
-    return text;
-    
+    parts: text,
+   });
 
 
    // Checking for empty messages
@@ -156,30 +147,4 @@ historyLog.push({
     console.log(e)
    }
 
-};
-
-/*
-
-    //Saves User input into history
-    historyLog.push({
-        role: "user",
-        parts: userInput,
-    });
-    const msg = userInput;
-    const result = await chat.sendMessage(msg);
-    const response = await result.response;
-    const text = response.text();
-    //Saves Ai output into history
-    historyLog.push({
-        role: "model",
-        parts: text,
-    });
-    // Limits history, Change limit to keep more history
-    let limit = 30;
-    if (historyLog.length >  limit) {
-        historyLog.shift();
-        historyLog.shift();
-    }
-    return text;
-    }
-*/
+});
